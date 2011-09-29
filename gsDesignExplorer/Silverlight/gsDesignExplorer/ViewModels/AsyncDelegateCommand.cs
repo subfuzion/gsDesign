35f8074e33c9610e7eef730dddf932dde86caada
Tony Pujals <tony@subfuzion.com>
2011-09-29 10:59:45 -0700

    Added DelegateCommand and AsyncDelegateCommand classes.

---
+++ gsDesignExplorer/Silverlight/gsDesignExplorer/ViewModels/AsyncDelegateCommand.cs
@@ -0,0 +1,84 @@
+﻿namespace gsDesign.Explorer.ViewModels
+{
+	using System;
+	using System.ComponentModel;
+
+	public class AsyncDelegateCommand : DelegateCommand
+	{
+		private readonly Action<object> _completedAction;
+		private readonly Action<Exception> _errorAction;
+		private readonly Action<object, ProgressChangedEventArgs> _progressAction;
+		private BackgroundWorker _worker;
+
+		public AsyncDelegateCommand(
+			Action<object> action,
+			Func<object, bool> canExecute = null,
+			Action<object> completed = null,
+			Action<object, ProgressChangedEventArgs> progress = null,
+			Action<Exception> error = null
+			)
+			: base(action, canExecute)
+		{
+			_completedAction = completed;
+			_progressAction = progress;
+			_errorAction = error;
+		}
+
+
+		public override void Execute(object parameter)
+		{
+			// shouldn't execute at all if CanExecute is queried properly first
+			if (IsBusy()) return;
+
+			_worker = new BackgroundWorker();
+			_worker.DoWork += (sender, doWorkEventArgs) =>
+			                  	{
+			                  		// uncomment for WPF
+			                  		// CommandManager.InvalidateRequerySuggested();
+			                  		base.Execute(parameter);
+			                  	};
+
+			_worker.ProgressChanged += (sender, progressChangedEventArgs) =>
+			                           	{
+			                           		if (_progressAction != null)
+			                           		{
+			                           			_progressAction(sender, progressChangedEventArgs);
+			                           		}
+			                           	};
+
+			_worker.RunWorkerCompleted += (sender, runWorkerCompletedEventArgs) =>
+			                              	{
+			                              		if (_completedAction != null)
+			                              		{
+			                              			if (runWorkerCompletedEventArgs.Error != null)
+			                              			{
+			                              				_errorAction(runWorkerCompletedEventArgs.Error);
+			                              			}
+			                              			else
+			                              			{
+			                              				_completedAction(runWorkerCompletedEventArgs.Result);
+			                              			}
+			                              		}
+
+			                              		_worker = null;
+			                              	};
+
+			_worker.RunWorkerAsync(parameter);
+		}
+
+		public override bool CanExecute(object parameter)
+		{
+			return base.CanExecute(parameter) && !IsBusy();
+		}
+
+		public void Cancel()
+		{
+			if (_worker != null && _worker.IsBusy && _worker.WorkerSupportsCancellation) _worker.CancelAsync();
+		}
+
+		public bool IsBusy()
+		{
+			return _worker != null && _worker.IsBusy;
+		}
+	}
+}
---
+++ gsDesignExplorer/Silverlight/gsDesignExplorer/ViewModels/DelegateCommand.cs
@@ -0,0 +1,49 @@
+﻿namespace gsDesign.Explorer.ViewModels
+{
+	using System;
+	using System.Windows.Input;
+
+	public class DelegateCommand : ICommand
+	{
+		private readonly Action<object> _action;
+		private readonly Func<object, bool> _canExecute;
+		private bool _canExecuteCached;
+
+		public DelegateCommand(Action<object> action, Func<object,bool> canExecute = null)
+		{
+			_action = action;
+			_canExecute = canExecute;
+		}
+
+		#region ICommand Members
+
+		public event EventHandler CanExecuteChanged;
+
+		public virtual bool CanExecute(object parameter)
+		{
+			bool canExecute = _canExecute == null || _canExecute(parameter);
+
+			// if the current value does not match the previously cached value,
+			// then fire CanExecuteChanged
+			if (_canExecuteCached != canExecute)
+			{
+				_canExecuteCached = canExecute;
+				RaiseCanExecuteChanged();
+			}
+
+			return canExecute;
+		}
+
+		public virtual void Execute(object parameter)
+		{
+			if (_action != null) _action(parameter);
+		}
+
+		#endregion
+
+		public void RaiseCanExecuteChanged()
+		{
+			CanExecuteChanged(this, EventArgs.Empty);
+		}
+	}
+}
