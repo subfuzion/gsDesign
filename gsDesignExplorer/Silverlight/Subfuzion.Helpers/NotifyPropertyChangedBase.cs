4fc27d4a528ab39fe8990e1a309446beeb07a5c3
Tony Pujals <tony@subfuzion.com>
2011-10-17 13:28:23 -0700

    Adding validations

--- gsDesignExplorer/Silverlight/Subfuzion.Helpers/NotifyPropertyChangedBase.cs
+++ gsDesignExplorer/Silverlight/Subfuzion.Helpers/NotifyPropertyChangedBase.cs
@@ -1,16 +1,20 @@
 namespace Subfuzion.Helpers
 {
 	using System;
+	using System.Collections;
+	using System.Collections.Generic;
 	using System.ComponentModel;
 	using System.Windows;
 
-	public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+	public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged, INotifyDataErrorInfo
 	{
-		#region INotifyPropertyChanged Members
+		#region INotifyPropertyChanged
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
 		#endregion
+
+		#region INotifyPropertyChanged implementation
 
 		protected void RaisePropertyChanged(string property)
 		{
@@ -33,5 +37,111 @@ namespace Subfuzion.Helpers
 				Console.WriteLine(e);
 			}
 		}
+
+		#endregion
+
+		#region INotifyDataErrorInfo
+
+		public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+		public IEnumerable GetErrors(string property)
+		{
+			if (_errors == null) return null;
+
+			if (string.IsNullOrEmpty(property))
+			{
+				// return the whole error collection
+				return _errors.Values;
+			}
+
+			if (_errors.ContainsKey(property))
+			{
+				return _errors[property];
+			}
+
+			return null;
+		}
+
+		public bool HasErrors
+		{
+			get { return _errors != null && _errors.Count > 0; }
+		}
+
+		#endregion
+
+		#region INotifyDataErrorInfo implementation
+
+		private Dictionary<string, List<string>> _errors;
+
+		protected void RaiseErrorsChanged(string property)
+		{
+			try
+			{
+				if (ErrorsChanged != null)
+				{
+					Deployment.Current.Dispatcher.BeginInvoke(() =>
+					{
+						// the double check is necessary in case anything has changed by the time this is invoked on the UI thread
+						if (ErrorsChanged != null)
+						{
+							ErrorsChanged(this, new DataErrorsChangedEventArgs(property));
+						}
+					});
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+
+		protected void AddError(string property, string error)
+		{
+			if (_errors == null) _errors = new Dictionary<string, List<string>>();
+
+			if (_errors.ContainsKey(property))
+			{
+				_errors[property].Add(error);
+			}
+			else
+			{
+				_errors.Add(property, new List<string> { error });
+			}
+
+			RaiseErrorsChanged(property);
+		}
+
+		protected void ClearErrors(string property = null)
+		{
+			if (_errors == null || _errors.Count == 0) return;
+
+			if (property != null)
+			{
+				_errors.Remove(property);
+				RaiseErrorsChanged(property);
+			}
+			else
+			{
+				foreach (var error in _errors)
+				{
+					ClearErrors(error.Key);
+				}
+
+				_errors = null;
+			}
+		}
+
+		protected void SetErrors(string property, List<string> errors)
+		{
+			if (_errors == null) _errors = new Dictionary<string, List<string>>();
+
+			_errors.Remove(property);
+			_errors.Add(property, errors);
+			RaiseErrorsChanged(property);
+		}
+
+		#endregion
+
+
 	}
 }
--- gsDesignExplorer/Silverlight/gsDesignExplorer/Models/Ept.cs
+++ gsDesignExplorer/Silverlight/gsDesignExplorer/Models/Ept.cs
@@ -3,62 +3,77 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Collections.ObjectModel;
+	using System.ComponentModel.DataAnnotations;
 	using Subfuzion.Helpers;
 
 	public class Ept : NotifyPropertyChangedBase
 	{
-		private double ComputeValidPower(double power, double error)
+		public Ept()
 		{
-			error = Math.Round(error, 1);
-
-			if (power - error < 0.0001)
-			{
-				power = error + 0.0001;
-			}
-
-			if (power > 100.0)
-			{
-				power = 100.0;
-			}
-
-			return Math.Round(power, 4);
-		}
-
-		// triggers notification if needs updating
-		private void UpdatePower()
-		{
-			if (Power - Error < 0.0001)
-			{
-				Power = ComputeValidPower(Power, Error);
-			}
+			Error = 2.5;
+			Power = 90.0;
 		}
 
 		#region Error property
 
 		private double _error;
 
+		[Display(Name = "Type I Error (1-sided \u03B1 x 100)",
+			Description = "0 < Type I Error < Power < 100")]
 		public double Error
 		{
 			get { return _error; }
 
 			set
 			{
-				if (Math.Abs(_error - value) >= 0.1)
+				if (Math.Abs(_error - value) >= double.Epsilon)
 				{
-					_error = Math.Round(value, 1);
+					if (!ValidateErrorValue(value))
+						throw new ArgumentException("Error must be a value between 0.0 and 100.0, inclusive");
+
+					_error = Math.Round(value, 4);
 					RaisePropertyChanged("Error");
+
 					UpdatePower();
 					RaisePropertyChanged("MinimumValidPower");
+					RaisePropertyChanged("MinimumPowerDisplay");
 				}
 			}
 		}
 
 		#endregion // Error
 
+		public bool ValidateErrorValue(double value)
+		{
+			return !(MinimumValidError - value > double.Epsilon || value - MaximumValidError > double.Epsilon);
+		}
+
+		#region MinimumValidError property
+
+		public double MinimumValidError
+		{
+			get { return 0.0; }
+		}
+
+		#endregion // MinimumValidError
+
+		#region MaximumValidError property
+
+		public double MaximumValidError
+		{
+			get { return 100.0; }
+		}
+
+		#endregion // MaximumValidError
+
+
+
 		#region Power property
 
 		private double _power;
 
+		[Display(Name = "Power (100 x [1-\u03B2])",
+			Description = "0 < Type I Error < Power < 100")]
 		public double Power
 		{
 			get { return _power; }
@@ -66,12 +81,17 @@
 			set
 			{
 				var power = ComputeValidPower(value, Error);
-				if (Math.Abs(_power - power) >= 0.0001)
+				if (Math.Abs(_power - power) > double.Epsilon)
 				{
 					_power = power;
-					RaisePropertyChanged("Power");
-					RaisePropertyChanged("MinimumValidPower");
 				}
+
+				// because of slider to spinner binding, we need this out
+				// here, otherwise slider doesn't always update properly
+				// (depending on when the mouse button is actually released)
+				RaisePropertyChanged("Power");
+				RaisePropertyChanged("MinimumValidPower");
+				RaisePropertyChanged("MinimumPowerDisplay");
 			}
 		}
 
@@ -81,27 +101,21 @@
 
 		public double MinimumValidPower
 		{
-			get { return Math.Min(Math.Round(Error + 0.0001, 4), MaximumValidPower); }
+			get { return Math.Min(Math.Round(Error + 0.1, 1), MaximumValidPower); }
 		}
 
 		#endregion // MinimumValidPower
 
 		#region MaximumValidPower property
 
-		private double _maximumValidPower = 100.0;
-
 		public double MaximumValidPower
 		{
-			get { return _maximumValidPower; }
+			get { return 100.0; }
+		}
 
-			set
-			{
-				if (Math.Abs(_maximumValidPower - value) > double.Epsilon)
-				{
-					_maximumValidPower = value;
-					RaisePropertyChanged("MaximumValidPower");
-				}
-			}
+		public string MinimumPowerDisplay
+		{
+			get { return string.Format("0.1 (current value={0}, minimum={1})", Power, MinimumValidPower); }
 		}
 
 		#endregion // MaximumValidPower
@@ -226,6 +240,29 @@
 
 		#endregion // IsTimingTableEnabled
 
+		#region Implementation
+
+		private double ComputeValidPower(double power, double error)
+		{
+			if (power - error < 0.1)
+			{
+				power = error + 0.1;
+			}
+
+			if (power > MaximumValidPower)
+			{
+				power = MaximumValidPower;
+			}
+
+			return Math.Round(power, 1);
+		}
+
+		// triggers notification if needs updating
+		private void UpdatePower()
+		{
+			Power = ComputeValidPower(Power, Error);
+		}
+
 		private void UpdateTimingTable()
 		{
 			if (TimingTable.Count < IntervalCount)
@@ -233,7 +270,7 @@
 				var increment = 0.5;
 				for (int i = TimingTable.Count; i < IntervalCount; i++)
 				{
-					TimingTable.Add(new Timing { Index = i+1, Value = increment});
+					TimingTable.Add(new Timing {Index = i + 1, Value = increment});
 				}
 			}
 			else if (TimingTable.Count > IntervalCount)
@@ -247,6 +284,8 @@
 
 			RaisePropertyChanged("TimingTable");
 		}
+
+		#endregion
 	}
 
 	public class Timing
