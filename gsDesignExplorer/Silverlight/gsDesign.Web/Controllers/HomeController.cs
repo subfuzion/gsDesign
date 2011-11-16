e03f471f06a6b8901f0a79e472a06e53d1b24662
Tony Pujals <tony@subfuzion.com>
2011-11-16 15:03:09 -0800

    Website deployment.

--- gsDesignExplorer/Silverlight/gsDesign.Web/Controllers/HomeController.cs
+++ gsDesignExplorer/Silverlight/gsDesign.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace gsDesign.Web.Controllers
 {
@@ -10,8 +6,6 @@ namespace gsDesign.Web.Controllers
 	{
 		public ActionResult Index()
 		{
-			ViewBag.Message = "Welcome to ASP.NET MVC!";
-
 			return View();
 		}
 
