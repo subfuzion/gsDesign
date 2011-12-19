e753b04b5b9e37cf15c67ba78965bedece8c5d35
Tony Pujals <tony@subfuzion.com>
2011-12-19 10:33:25 -0800

    Updating gitignore, moved thirdparty resources.

--- gsDesignExplorer/Silverlight/thirdparty/mongoose/build.bat
+++
@@ -1,5 +0,0 @@
-set MINGWDBG=-DNDEBUG -Os
-set MINGWOPT=-W -Wall -mthreads -Wl,--subsystem,windows %MINGWDBG%
-
-gcc %MINGWOPT% mongoose.c -lws2_32 -shared -Wl,--out-implib=mongoose.lib -o _mongoose.dll
-gcc %MINGWOPT% mongoose.c main.c win32\res.o -lws2_32 -ladvapi32 -o mongoose.exe
